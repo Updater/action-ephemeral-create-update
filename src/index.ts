663a59b87987522f37770edb40046c08a47ab9d1
apollorion <joey@apollorion.com>
2022-02-28 16:36:55 -0500

    regex is hard

--- src/index.ts
+++ src/index.ts
@@ -74,7 +74,7 @@ async function run() {
 }
 
 function dnsSafe(s: string): string{
-    return s.replace(new RegExp("_","gm"), "-").replace(new RegExp(".","gm"), "-");
+    return s.replace(/_/g, "-").replace(/\./g, "-");
 }
 
 run();
